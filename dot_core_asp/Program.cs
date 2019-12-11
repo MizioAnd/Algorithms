91fdc80a03f1ad91d23ade8c6308ea476e0b84df
Andersen <mizio.spatafora@gmail.com>
2019-12-11 23:35:49 +0100

    Add class with Main method.

---
+++ dot_core_asp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using dot_core_asp.Models;
+
+namespace dot_core_asp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // var dfs = new DFS();
+            // dfs.TestDFS();
+            var bfs = new BFS();
+            bfs.TestBFS();
+
+            // DicesSumModel diceSumModel = new DicesSumModel();
+            // diceSumModel.TestProbabilityDiceSum();
+
+            // DateTimeModel dateTimeModel = new DateTimeModel();
+            // dateTimeModel.TestMultipleDateTimeDiff();
+
+            // Build webhost
+            // BuildWebHost(args).Run();
+        }
+
+        // public static IWebHost BuildWebHost(string[] args) =>
+        //     WebHost.CreateDefaultBuilder(args)
+        //         .UseStartup<Startup>()
+        //         .Build();
+    }
+}
