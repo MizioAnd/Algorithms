52d9a39bf6eacc52975242652caca5dd629b8376
Andersen <mizio.spatafora@gmail.com>
2019-12-11 22:15:57 +0100

    Add all adjacent nodes on same level to stack and mark as visited.

--- dot_core_asp/Models/BFS.cs
+++ dot_core_asp/Models/BFS.cs
@@ -6,8 +6,37 @@ namespace dot_core_asp.Models
 {
     public class BFS
     {
-        public void BFSIterative(Graph graph, Node node)
+        public void BFSIterative(Graph G, Node s)
         {
+            Stack StackElements = new Stack();
+            StackElements.Push(s.ResidingNode);
+
+            s.ResidingNode.Visited = true;
+            G.PrintVisitedVertices();
+
+            while (StackElements.Count != 0)
+            {
+                // Assume that a node can have many children. Define new type child and create List<Vertex> for all children with increasing indexing.
+                var v = StackElements.Top();
+                var nodeOfv = G.GetNode(v);
+
+                foreach (var child in nodeOfv.Children)
+                {
+                    if (!child.Visited)
+                    {
+                        StackElements.Push(child);
+                        child.Visited = true;
+                    }
+                    else
+                    {
+                        StackElements.Push(G.GetNode(child).Parent);
+                        G.GetNode(child).Parent.Visited = true;
+                    }
+                }
+
+            }
+
+
             throw new NotImplementedException();
         }
 
