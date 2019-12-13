9ecdd4d0390043145c3d58ee734d675752a37dad
Andersen <mizio.spatafora@gmail.com>
2019-12-13 13:24:06 +0100

    Implement Fibonacci iterative method.

--- dot_core_asp/Models/Fibonacci.cs
+++ dot_core_asp/Models/Fibonacci.cs
@@ -25,6 +25,20 @@ namespace dot_core_asp.Models
             return result;
         }        
 
+        public void FibonacciIterative(int s)
+        {
+            var range = Enumerable.Range(0, s+1);
+            var sums = new int[s+1];
+            foreach (var ite in range)
+            {
+                if (ite == 0 | ite == 1)
+                    sums[ite] = ite;
+                else
+                    sums[ite] = sums[ite-1] + sums[ite-2];
+            }
+            Console.WriteLine(String.Join("; ", sums));
+        }
+
         public void TestFibonacci()
         {
             int input = 3;
@@ -33,6 +47,7 @@ namespace dot_core_asp.Models
             foreach (var ite in inputFibo)
                 Console.WriteLine(FibonacciRecursive(ite));
             // Console.WriteLine(String.Format("Fibonacci result: {0} from input s={1}", result, input));
+            FibonacciIterative(9);
         }
     }
 }
